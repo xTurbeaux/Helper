d976dad9ca3b96cedb304af8a7cacb68a3171b89
aaron mayhew <aaronmayhew@aarons-MBP-2.attlocal.net>
2019-05-02 00:38:49 -0500

    setting up user model and validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route    Get api/users
-// @desc     Test route
+const User = require('../../models/User');
+
+// @route    POST api/users
+// @desc     Register User
 // @access   Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is Required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      // see if user exists
+
+      // get users gravatar
+
+      //encrypt password
+
+      //return jsonwebtoken
+      res.send('User route');
+    } catch (err) {}
+  }
+);
 
 module.exports = router;
