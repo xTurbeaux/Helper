f1bf2d74f72f0e7f1218c67b8908b1199b08dab8
Aaron Mayhew <standingbytocopy@gmail.com>
2019-08-08 20:49:26 -0500

    set up alert system on failed password match

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
 import { Provider } from 'react-redux';
 import store from './store';
 import './App.css';
@@ -15,6 +16,7 @@ const App = () => (
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
