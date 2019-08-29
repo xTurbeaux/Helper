0526264004f677a9668d81b5a3930ff530767c33
Aaron Mayhew <standingbytocopy@gmail.com>
2019-08-28 20:45:41 -0500

    auth for user and profiles added in redux

---
+++ client/src/utils/setAuthToken.js
@@ -0,0 +1,11 @@
+import axios from 'axios'; //adding a global header
+
+const setAuthToken = token => {
+  if (token) {
+    axios.defaults.headers.common['x-auth-token'] = token;
+  } else {
+    delete axios.defaults.headers.common['x-auth-token'];
+  }
+};
+
+export default setAuthToken;
